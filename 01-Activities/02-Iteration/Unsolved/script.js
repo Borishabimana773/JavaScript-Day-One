5bfb69b19a9790b4460ac64494b1c91326c27ec0
Borishab <habimana40@gmail.com>
2022-01-04 21:06:50 -0500

    initial commit

---
+++ 01-Activities/02-Iteration/Unsolved/script.js
@@ -0,0 +1,7 @@
+// TODO: Write Your JavaScript Code Here
+var students = ['Sarah', 'Orlando', 'Heather', 'Ismael', 'Hung'];
+console.log(students.length);
+
+for (var i=0; i<students.length; i++) {
+    console.log('Great to see you, '+ students[i] + '!');
+}
